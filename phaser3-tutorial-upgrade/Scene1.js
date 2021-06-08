9988573f380a485069709c010f2482be69505303
nicolasnocete <nicolasnocete@gmail.com>
2021-06-08 12:05:35 -0300

    runner

--- phaser3-tutorial-upgrade/Scene1.js
+++ phaser3-tutorial-upgrade/Scene1.js
@@ -6,10 +6,7 @@ class Scene1 extends Phaser.Scene {
     preload ()
     {
       this.load.image('logo', 'assets/logo.png');
-      this.load.image('sky', 'assets/sky.png');
-      this.load.image('ground', 'assets/platform.png');
-      this.load.image('star', 'assets/star.png');
-      this.load.image('bomb', 'assets/bomb.png');
+
       this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });      
     }
 
---
+++ runner/game.js
@@ -0,0 +1,32 @@
+let game;
+
+let config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1334,
+        height: 750,
+      },
+    
+    scene: [playGame],
+    backgroundColor: 0x444444,
+
+    // physics settings
+    physics: {
+        default: "arcade"
+    }
+}
+
+game = new Phaser.Game(config);
+
+// global game options
+let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
+}
